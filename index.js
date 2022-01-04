751e4efb155ab7d01d4568e8408111563ad6c33f
Greybph <andrewlfauth@gmail.com>
2022-01-04 12:40:44 -0800

    removed unused modules
    now that routing is done via controllers in seperate files, there is no need to require our DB Models or mongoose in our index.js file

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose')
-const Todos = require('./models/Todos')
-const Completed = require('./models/Completed')
 require('dotenv').config()
 const PORT = process.env.PORT || 4000
 const homePageController = require('./controllers/homePage')
@@ -11,17 +8,11 @@ const todoDoneController = require('./controllers/todoDone')
 const completedPageController = require('./controllers/completedPage')
 const deleteCompletedTodo = require('./controllers/deleteCompletedTodo')
 
-const dbConnect = require('./utils/dbConnect')
+require('./utils/dbConnect')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: false }))
-
-// mongoose.connect(process.env.MONGODB_URI, {
-// 	useNewUrlParser: true,
-// 	useUnifiedTopology: true,
-// })
-dbConnect
 
 app.get('/', homePageController)
 
